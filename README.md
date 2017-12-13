3981d7763018edcaf2de0aecb58150b002de167d
Anton Relin <arelin@sas.upenn.edu>
2017-12-13 17:53:06 -0500

    compare updated

--- README.md
+++
@@ -1,2 +0,0 @@
-# CIS450-Project-HALA
-Hannah, Alice, Lissa, and Anton CIS 450 Project
--- csvtojson.py
+++
@@ -1,9 +0,0 @@
-import csv
-import json
-
-with open('match_database5.csv') as f:
-    reader = csv.DictReader(f)
-    rows = list(reader)
-
-with open('matchconverted5.json', 'w') as f:
-    json.dump(rows, f)
